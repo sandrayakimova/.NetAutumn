856bc57c7f192fb1618d5b0928370767e87e3f3b
sandrayakimova <43165224+sandrayakimova@users.noreply.github.com>
2019-08-27 23:00:25 +0300

    Add files via upload

---
+++ NET.A.2019.YAKIMOVA06/Polinomus.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace day6z1
+{
+    public class Polinomus
+    {
+       public  int degree;
+       public int[] coef;
+
+    
+        public override string ToString() //<example> degree=3 coef={4,5,6,7} polin="4x^3+5x^2+6x+7
+        {
+            string polin=null;
+            for (int i = 0; i<degree-1;i++)
+                {
+                if (coef[i]==0) { continue; }
+                polin += Convert.ToString(coef[i]) + "x^" + Convert.ToString(degree - i)+"+";
+
+            }
+            polin += Convert.ToString(coef[degree-1])+"x+" + Convert.ToString(coef[degree]);
+
+            return polin;
+        }
+        public static bool operator ==(Polinomus pol1,Polinomus pol2) //we compare there degree and coefitients 
+        {
+            if (pol1.degree!=pol2.degree) { return false; }
+            else
+            {
+                for (int i=0;i<=pol2.degree;i++)
+                {
+                    if (pol1.coef[i]!=pol2.coef[i]) { return false; }
+                }
+                return true;
+            }
+        }
+        public static bool operator !=(Polinomus pol1, Polinomus pol2)
+        {
+            if (pol1.degree != pol2.degree) { return true; }
+            else
+            {
+                for (int i = 0; i <= pol2.degree; i++)
+                {
+                    if (pol1.coef[i] != pol2.coef[i]) { return true; }
+                }
+                return false;
+            }
+
+        }
+        public static Polinomus operator + (Polinomus pol1, Polinomus pol2) //when you sum 2 polinoms you create new polinom with sum of their coeffitients
+        {
+            Polinomus otv = new Polinomus();
+            if (pol1.degree > pol2.degree)
+            {
+                for (int i = 0; i <= pol1.degree; i++)
+                {
+                    if (i <= pol2.degree)
+                    {
+                        otv.coef[i] = pol1.coef[i] + pol2.coef[i];
+                    }
+                    else { otv.coef[i] = pol1.coef[i]; }
+                }
+            } else
+            {
+                for (int i = 0; i <= pol2.degree; i++)
+                {
+                    if (i <= pol1.degree)
+                    {
+                        otv.coef[i] = pol1.coef[i] + pol2.coef[i];
+                    }
+                    else { otv.coef[i] = pol2.coef[i]; }
+                }
+            }
+            return otv;
+        }
+
+    }
+}
---
+++ NET.A.2019.YAKIMOVA06/Program.cs
@@ -0,0 +1,232 @@
+﻿using System;
+
+namespace day6z2
+{
+    class MainClass
+    {
+         public struct Fl
+            /*сформировали структуру  чтобы потом сформировать импровизированную
+             * таблицу из номера строки ,суммы строки ,максимального элемента  и тд */
+        {
+            public int index;
+            public int sum;
+            public int max;
+             public int min;
+           
+
+        }
+        public static Fl []  FormFlags(int[] [] temp)
+        {
+            //сформируем эту таблицу
+            
+            int t = temp.GetLength(0);
+            Fl[] s = new Fl[t];
+            for (int i = 0; i < temp.GetLength(0); i++)
+            {
+                
+                int sum = 0;
+                int max=temp[i][0];
+                int min=temp[i][0];
+
+                for (int j = 0; j < temp[i].Length; j++)
+                {
+
+                    sum += temp[i][ j];
+                    if (max<temp[i][j])
+                    {
+                        max = temp[i][ j];
+                    }
+                    if (min>temp[i][j])
+                    {
+                        min = temp[i][j];
+                    }
+                }
+                
+
+                s[i].index = i;
+                
+                s[i].sum = sum;
+                
+                s[i].max = max;
+           
+                s[i].min = min;
+                Console.Write(s[i].index + " " + s[i].sum + " " + s[i].max + " " + s[i].min);
+                Console.WriteLine();
+
+
+            }
+            return s;
+            
+        }
+        
+        public static void FormOrderedArray(int[][]temp , Fl[] s)
+        {
+            //после того когда мы нашу импровизированную таблицу отсортировали по нужному параметру нам нужно воссоздать по ней матрицу
+            int[][] otv = new int[temp.Length] [];
+
+            for (int i = 0; i < temp.GetLength(0); i++)
+            {
+                
+                for (int j = 0; j < temp[s[i].index].Length; j++)
+                    
+                {
+                    int ind = s[i].index;
+                    Console.Write(temp[ind][j]+" ");
+                    
+                }
+                Console.WriteLine();
+            }
+          
+        }
+      /*  public static int[][] OrderbysumGr(int[][] temp,Fl[] s)
+        {
+            //сортировка по возрастанию по сумме
+
+            
+            Fl sw;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i].sum > s[j].sum)
+                    {
+                        sw = s[i];
+                        s[i] = s[j];
+                        s[j] = sw;
+                    }
+                }
+            }
+            return FormOrderedArray(temp, s);
+        }
+
+        public static int[][] OrderbysumLw(int[][] temp, Fl[] s)
+        {
+            //сортировке по убыванию по сумме
+
+            Fl sw;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i].sum < s[j].sum)
+                    {
+                        sw = s[i];
+                        s[i] = s[j];
+                        s[j] = sw;
+                    }
+                }
+            }
+            return FormOrderedArray(temp, s);
+        }
+        */
+        public static void OrderbyMaxGr(int[] [] temp, Fl[] s)
+        {
+            //сортировка по возрастанию по макс элементу
+            Fl sw;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i].max > s[j].max)
+                    {
+                        sw = s[i];
+                        s[i] = s[j];
+                        s[j] = sw;
+                    }
+                }
+            }
+            /*for (int i = 0; i < s.Length;i++)
+            {
+                Console.Write(s[i].index + " " + s[i].sum + " " + s[i].max + " " + s[i].min);
+                Console.WriteLine();
+            }
+            OutputArray(temp);
+            */
+            FormOrderedArray(temp, s);
+        }
+       /* public static int[][] OrderbyMaxLw(int[][] temp, Fl[] s)
+        {
+            //сортировка по убыванию по макс элементу
+            Fl sw;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i].max < s[j].max)
+                    {
+                        sw = s[i];
+                        s[i] = s[j];
+                        s[j] = sw;
+                    }
+                }
+            }
+            return FormOrderedArray(temp, s);
+        }
+        public static int[][] OrderbyMinGr(int[][] temp, Fl[] s)
+        {
+            //сортировка по мин элементу по возрастанию
+            Fl sw;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i].min > s[j].min)
+                    {
+                        sw = s[i];
+                        s[i] = s[j];
+                        s[j] = sw;
+                    }
+                }
+            }
+            return FormOrderedArray(temp, s);
+        }
+
+        public static int[][] OrderbyMinLw(int[][] temp, Fl[] s)
+        {
+            //сортировка по мин элементу по убыванию
+            Fl sw;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i].min < s[j].min)
+                    {
+                        sw = s[i];
+                        s[i] = s[j];
+                        s[j] = sw;
+                    }
+                }
+            }
+            return FormOrderedArray(temp, s);
+        } */
+         public static void OutputArray(int[] [] Temp)
+        {
+            for (int i = 0; i < Temp.GetLength(0); i++)
+            {
+                for (int j = 0; j < Temp[i].Length; j++)
+                {
+                    Console.Write(Temp[i][ j] + " ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+   
+        public static void Main(string[] args)
+        {
+            int[][] nums = new int[3][];
+            nums[0] = new int[4] { 1, 2,8,9 };          
+            nums[1] = new int[3] { 0, 7, 4 };       
+            nums[2] = new int[5] { 3, 2, 3, 4, 5 };
+            Console.WriteLine("Исходный массив:");
+            OutputArray(nums);
+            Fl[] flags = FormFlags(nums);
+            Console.WriteLine("сортировка по возрастанию по сумме:");
+            OrderbyMaxGr(nums, flags);
+
+
+
+        }
+    }
+}
